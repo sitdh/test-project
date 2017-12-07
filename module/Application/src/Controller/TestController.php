8108dd5ee99d7bacc6fd09e56151625620808704
Sitdhibong Laokok <sitdhibong@gmail.com>
2017-12-07 20:52:43 +0700

    Append message

--- module/Application/src/Controller/TestController.php
+++ module/Application/src/Controller/TestController.php
@@ -8,17 +8,41 @@ use Application\EquationSolver\EquationSolver;
 
 use Application\Sequence\SequenceGenerator;
 
+use Zend\Cache\PatternFactory;
+
 class TestController extends AbstractActionController
 {
-    public function indexAction()
-    {
-      $e = SequenceGenerator::element([3, 5, 9, 15, 'X']);
-      $solver = new EquationSolver('(Y + 24) + (10 x 2) = 99', 'y');
+  private static $sequenceCache;
 
-      return new ViewModel([
-        'sequence'  => $e,
-        'answer'    => $solver->solve(),
-      ]);
-    }
+  private static $solverCache;
+
+  public function __construct() 
+  {
+    TestController::$solverCache = new SequenceGenerator();
+    // $seq = new SequenceGenerator();
+    // $this->sequenceCache = PatternFactory::factory('object', [
+    //   'object'                => $seq,
+    //   'object_key'            => 'sequence',
+    //   'storage'               => 'apc',
+    //   'object_cache_methods'  => ['element'],
+    // ]);
+
+    TestController::$sequenceCache = new EquationSolver();
+    // $solver = new EquationSolver();
+    // $this->solverCache = PatternFactory::factory('object', [
+    //   'object'                => $solver,
+    //   'object_key'            => 'solver',
+    //   'storage'               => 'apc',
+    //   'object_cache_methods'  => ['solve'],
+    // ]);
+  }
+  public function indexAction()
+  {
+    $solver = new EquationSolver();
+    return new ViewModel([
+      'sequence'  => TestController::$solverCache->element([3,5,9,15, 'x']),
+      'answer'    => TestController::$sequenceCache->solve('(Y + 24) + (10 x 2) = 99', 'y'),
+    ]);
+  }
 }
 
