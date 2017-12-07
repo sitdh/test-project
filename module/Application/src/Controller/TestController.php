8fad94f8b8bf1ba259c66079ad1954bd11280671
Sitdhibong Laokok <sitdhibong@gmail.com>
2017-12-07 20:05:10 +0700

    Attach sequence gen

--- module/Application/src/Controller/TestController.php
+++ module/Application/src/Controller/TestController.php
@@ -6,14 +6,18 @@ use Zend\View\Model\ViewModel;
 
 use Application\EquationSolver\EquationSolver;
 
+use Application\Sequence\SequenceGenerator;
+
 class TestController extends AbstractActionController
 {
     public function indexAction()
     {
+      $e = SequenceGenerator::element([3, 5, 9, 15, 'X']);
       $solver = new EquationSolver('(Y + 24) + (10 x 2) = 99', 'y');
 
       return new ViewModel([
-        'answer'      => $solver->solve(),
+        'sequence'  => $e,
+        'answer'    => $solver->solve(),
       ]);
     }
 }
